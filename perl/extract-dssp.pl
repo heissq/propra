0e53530fe86ebf0a7fe592c54986c34764df728d
carstenuhlig <carsten.uhlig@gmail.com>
2013-02-27 15:43:38 +0100

    file ausgabe von dssp->input für programm mit liste funktionsfähig

--- perl/extract-dssp.pl
+++ perl/extract-dssp.pl
@@ -46,74 +46,108 @@ if ( $mode eq 'pdb' ) {
 }
 
 if ( $mode eq 'dssp' ) {
+
    # do dssp mode
 
    # files suchen nach pdb ids
-   my $array_ref = getFiles($dssp_folder);
-   @file_protein_list = @{$array_ref};
+   my $array_ref = getFiles( $dssp_folder, \@pdb_id_list );
+@file_protein_list = @{$array_ref};
 
-   # files einlesen
-   print "##########\nDSSP-Modus:\n##########\n\n\n";
-   foreach (@file_protein_list) {
-      my $reference    = '';
-      my $source       = '';
-      my $aa_sequence  = ();
-      my $sec_sequence = ();
-      my $istable      = 0;
-      my $pdbid        = '';
+# files einlesen
+foreach (@file_protein_list) {
+   my $reference     = '';
+   my $source        = '';
+   my $aa_sequence   = ();
+   my $sec_sequence  = ();
+   my $istable       = 0;
+   my $pdbid         = '';
+   my @aa_sequences  = ();
+   my @sec_sequences = ();
+   my $last_line = '';
+   my @id_seq = ();
+   my $seq_break_count = 0;
+   my $last_seq_id = '';
 
-      open FILE, "<$_" or die $!;
+   open FILE, "<$_" or die $!;
 
-      #do processing
-      while ( my $line = <FILE> ) {
+   #do processing
+   while ( my $line = <FILE> ) {
 
-         #Referenz:
-         if ( $line =~ m/^REFERENCE (.*).$/ ) {
-            $reference .= $1;
-         }
-
-         #Source:
-         if ( $line =~ m/^SOURCE    (.*).$/ ) {
-            $source .= $1;
-         }
-
-         #Header:
-         if ( $line =~ m/^HEADER    .*(\w{3,4})\s+.$/ ) {
-            $pdbid .= $1;
-         }
-
-         #erkennung der tabelle:
-         if ( $line =~ m/^\s+#\s+RESIDUE/ ) {
-            $istable = 1;
-         }
-
-         #hinzufügen von aa_sequenz und sec_sequenz:
-         if ( $istable && $line =~ m/^.{13}([A-Z])..([A-Z| ])/ ) {
-            my $tmp_sec = '';
-
-            if ( $2 eq 'E' || $2 eq 'B' ) {
-               $tmp_sec = 'E';
-            }
-
-            elsif ( $2 eq 'G' || $2 eq 'H' ) {
-               $tmp_sec = 'H';
-            }
-            else {
-               $tmp_sec = 'C';
-            }
-            $aa_sequence  .= $1;
-            $sec_sequence .= $tmp_sec;
-         }
+      #Referenz:
+      if ( $line =~ m/^REFERENCE (.*).$/ ) {
+         $reference .= $1;
       }
 
-      close FILE;
-      print "Referenz:\n$reference\n";
-      print "PDB-Id:\n$pdbid\n";
-      print
-      "Quelle:\n$source\n\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\n";
-      print "  $aa_sequence\n";
-      print "  $sec_sequence\n";
+      #Source:
+      if ( $line =~ m/^SOURCE    (.*).$/ ) {
+         $source .= $1;
+      }
+
+      #Header:
+      if ( $line =~ m/^HEADER    .*(\w{4,})\s+.$/ ) {
+         $pdbid = $1;
+      }
+
+      #hinzufügen von aa_sequenz und sec_sequenz:
+      if ( $istable && $line =~ m/^.{13}([A-Z])..([A-Z| ])/ ) {
+         my $tmp_sec = '';
+
+         if ( $2 eq 'E' || $2 eq 'B' ) {
+            $tmp_sec = 'E';
+         }
+         elsif ( $2 eq 'G' || $2 eq 'H' ) {
+            $tmp_sec = 'H';
+         }
+         else {
+            $tmp_sec = 'C';
+         }
+         $aa_sequence  .= $1;
+         $sec_sequence .= $tmp_sec;
+      }
+      if ( $line =~ m/!\*/) {
+         push @aa_sequences,  $aa_sequence;
+         push @sec_sequences, $sec_sequence;
+         $aa_sequence = '';
+         $sec_sequence = '';
+         $last_line =~ /^.{11}(\w)/;
+         $last_seq_id = $1;
+      }
+
+      #erkennung der tabelle:
+      if ( $line =~ m/^\s+#/ ) {
+         $istable = 1;
+      }
+
+      # pdbid einfügen
+      if ($last_line =~  m/^.{11}\s.+!\*/) { 
+      # für new chain
+        $seq_break_count = 0;
+        push @id_seq,"$pdbid$last_seq_id";
+      } elsif ( $last_line =~  m/^.{11}\s.+! / ) {
+      # für chainbreak
+      #my $tmp_seq_break_count = '';
+      #$tmp_seq_break_count = sprintf ("%02d",int($seq_break_count));
+      push @id_seq,"$pdbid$last_seq_id";
+      #$seq_break_count++;
+      }
+      $last_line = $line;
    }
+   # letzte sequenz in arrays
+   push @aa_sequences,  $aa_sequence;
+   push @sec_sequences, $sec_sequence;
+   $last_line =~ /^.{11}(\w)/;
+   push @id_seq,"$pdbid$1";
+
+
+   # in file einlesen die als dssp paramter angeben ist
+   open FILEPRINT, ">$dssp_file" or die "kann datei $dssp_file nicht erstellen\n";
+   for my $i ( 0 .. $#aa_sequences ) {
+      print FILEPRINT "\> $id_seq[$i]\n";
+      print FILEPRINT "AS $aa_sequences[$i]\n";
+      print FILEPRINT "SS $sec_sequences[$i]\n\n";
+   }
+   close FILEPRINT;
+}
 }
 
 if ( $mode eq 'cmp' ) {
@@ -136,22 +170,27 @@ if ( $dssp_bin ne '' ) {
 
 # suchen der dateien
 sub getFiles {
-   my @file_list = ();
+   my @pdb_id_list_tmp = @{ $_[1] };
+   my @file_list       = ();
+   my @missing_pdbids = ();
+   # TODO filedownload when no file for pdbid
+   # -aufgabenblatt 3 aufgabe 10
 
    opendir( DIR, "$_[0]" ) or die "$_[0]\/\n";
    while ( my $f = readdir(DIR) ) {    # dssp- oder pdb-ordner
       if ( $f !~ m/^\./ && -d "$_[0]\/$f\/" ) {
+
          # werden versteckte (.dateiname) dateien nicht aufgerufen
          opendir( SUBDIR, "$_[0]\/$f\/" )
             or die "ordner nicht vorhanden unterordner:$f\n";
          while ( my $sf = readdir(SUBDIR) ) {    # unterodnern
-            if ($_[0] =~ m/DSSP/ ) {
+            if ( $_[0] =~ m/DSSP/ ) {
 
                # dssp mode
-               for (@file_list) {
-                  if ( $sf =~ m/^(.{3,5}).*\.dssp$/ ) {
+               for (@pdb_id_list_tmp) {
+                  if ( $sf =~ m/(\w{4,})\.dssp$/ ) {
                      if ( $1 eq $_ ) {
-                        push @file_list,$sf;
+                        push @file_list, "$_[0]\/$f\/$sf";
                      }
                   }
                }
@@ -159,9 +198,9 @@ sub getFiles {
             else {
                # pdb mode
                for (@file_list) {
-                  if ( $sf =~ m/^pdb(.{3,5}).*\.ent$/ ) {
+                  if ( $sf =~ m/^pdb(.{4,})\.ent$/ ) {    #
                      if ( $1 eq $_ ) {
-                        push @file_list,$sf;
+                        push @file_list, "$_[0]\/$f\/$sf";
                      }
                   }
                }
@@ -174,7 +213,6 @@ sub getFiles {
    closedir(DIR);
    return \@file_list;
 }
-
 
 =begin GHOSTCODE
 opendir(DIR,$opts{d}) or die ": ordner nicht gefunden $opts{n}\n";
--- perl/folderreadindsspfiles.pl
+++ perl/folderreadindsspfiles.pl
@@ -4,21 +4,24 @@ use Getopt::Std;
 my %opts;
 getopts( 'n:l:o:', \%opts );
 
+
 $limit   = $opts{l};
 $counter = 0;
 $nr = $opts{n};
 
 open FILE, ">$opts{o}" or die "klappt nicht mit dem erstellen $!\n";
-opendir( DIR, "$opts{i}\/" ) or die "$opts{i}\/\n";
-while ( my $f = readdir(DIR) ) {    # homstrad ordner
+opendir( DIR, '/home/proj/biocluster/praktikum/bioprakt/Data/DSSP/' ) or die "$opts{i}\/\n";
+while ( my $f = readdir(DIR) ) {    # dssp oder pdb ordner
     if ( $f !~ m/^\./ && -d "$opts{i}\/$f\/" ) {
 
         # werden versteckte (.dateiname) dateien nicht aufgerufen
-        opendir( SUBDIR, "$opts{i}\/$f\/" )
+        opendir( SUBDIR, "\/home\/proj\/biocluster\/praktikum\/bioprakt\/Data\/DSSP\/$f\/" )
           or die "ordner nicht vorhandenunterordner:$f\n";
         while ( my $sf = readdir(SUBDIR) ) {    #unterodern = domäne
-            if ( $sf =~ m/^(.{3,5}\.dssp$/ ) {
+            if ( $sf =~ m/^(\w{4,})\.dssp$/ ) {
+               print $sf;
                 if ( $counter < $limit && int(rand($nr)) >= 10) {
+
                     print FILE "$sf";
                     $counter++;
                 }
