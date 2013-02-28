abe1c67005cbd8405729ad0c6346ebba6b98065c
carstenuhlig <carsten.uhlig@gmail.com>
2013-03-01 00:02:39 +0100

    summary nochmal erweitert. kann nun theoretisch in filename nach spezifikationen einen datensatz in txt format ausgeben v0.2

--- src/validation/CreateSummary.java
+++ src/validation/CreateSummary.java
@@ -7,23 +7,28 @@ import java.io.IOException;
 import gorpack.*;
 
 public class CreateSummary {
+	// id
 	private String id = "";
+	
 	// parameter to validate:
 	private double q3 = -1;
 	private double sov = -1;
-	private double qh = -1;
+	private double qh = -1; // TODO die drei q dinger berechnen
 	private double qe = -1;
 	private double qc = -1;
 	private double sov_h = -1;
 	private double sov_e = -1;
 	private double sov_c = -1;
 	
+	// filename
+	private String filename = "";
+
 	// strings a und b für aa-seq
-	private String a = "";
-	private String b = "";
+	private String as = "";
+	private String ss = "";
 	private String ps = "";
 
-	public CreateSummary(String id, String a, String b, String ps, double q3,
+	public CreateSummary(String file, String id, String as, String ss, String ps, double q3,
 			double sov, double qh, double qe, double qc, double sov_h,
 			double sov_e, double sov_c) {
 		this.id = id;
@@ -35,17 +40,29 @@ public class CreateSummary {
 		this.sov_h = sov_h;
 		this.sov_e = sov_e;
 		this.sov_c = sov_c;
-		this.a = a;
-		this.b = b;
+		this.as = as;
+		this.ss = ss;
 		this.ps = ps;
 	}
 
 	public void createFile() throws IOException {
-		try { //TODO format richtig ansetzen
-			// Create file
-			FileWriter fstream = new FileWriter("out.txt");
+		try { // TODO format richtig ansetzen --> frage wie viele sequenzen?
+				// Create file
+			FileWriter fstream = new FileWriter(filename);
 			BufferedWriter out = new BufferedWriter(fstream);
-			out.write("Hello Java");
+			
+			//erste zeile identifier
+			out.write(">"+id+" "+q3+" "+sov+" "+qh+" "+qe+" "+qc+" "+sov_h+" "+sov_e+" "+sov_c+"\n");
+			
+			//amino acid chain
+			out.write("AS "+as+"\n");
+			
+			//predicted secondary structure chain
+			out.write("PS "+ps+"\n");
+			
+			//secondary structure chain
+			out.write("SS "+ss+"\n");
+			
 			// Close the output stream
 			out.close();
 		} catch (Exception e) {// Catch exception if any
